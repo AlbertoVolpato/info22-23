beda2fe4b2f3902126b68bcc5675d4f602f6f85f
AlbertoVolpato <71636458+AlbertoVolpato@users.noreply.github.com>
2022-09-29 14:40:13 +0200

    Update index.js

--- fastify_rest_api/Pinacoteca/routes/index.js
+++ fastify_rest_api/Pinacoteca/routes/index.js
@@ -18,9 +18,25 @@ async function routes(fastify, options) {
             throw new Error(err)
         }
     });
-    //GET ONLY ONE USER;
-    fastify.get('/pittore/id', async function (request, reply) {
-
+    //GET ONLY ONE PITTORE;
+    fastify.get('/pittori/:id', async function (request, reply) {
+        const id = request.params.id
+        try {
+            const { rows } = await client.query('SELECT * FROM pittori WHERE cognome = $1', [id])
+            reply.send(rows)
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+    //GET ONLY ONE STENZA;
+    fastify.get('/stanze/:id', async function (request, reply) {
+        const id = request.params.id
+        try {
+            const { rows } = await client.query('SELECT * FROM stanze WHERE numero = $1', [id])
+            reply.send(rows)
+        } catch (err) {
+            throw new Error(err)
+        }
     })
     //POST A NEW PITTORE
     fastify.post('/pittori', async function (request, reply) {
@@ -36,6 +52,7 @@ async function routes(fastify, options) {
             const { rows } = await client.query(query)
             console.log(rows[0])
             reply.code(201)
+            reply.send("POST REQUEST DONE")
             return { created: true }
         } catch (err) {
             reply.send(err)
@@ -56,11 +73,71 @@ async function routes(fastify, options) {
             const { rows } = await client.query(query)
             console.log(rows[0])
             reply.code(201)
+            reply.send("POST REQUEST DONE")
             return { created: true }
         } catch (err) {
             reply.send(err)
         }
     });
-    //DELATE AN USER
+    //DELATE A PITTORE
+    fastify.delete('/pittori/:id', async function (request, reply) {
+        console.log(request.params)
+        try {
+            const { rows } = await client.query('DELETE FROM pittori WHERE Nome = $1', [request.params.id])
+            console.log(rows[0])
+            reply.code(204)
+            reply.send("DELETE REQUEST DONE")
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+    //DELETE A STANZA
+    fastify.delete('/stanze/:id', async function (request, reply) {
+        console.log(request.params)
+        try {
+            const { rows } = await client.query('DELETE FROM stanze WHERE numero = $1', [request.params.id])
+            console.log(rows[0])
+            reply.code(204)
+            reply.send("DELETE REQUEST DONE")
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+    //EDIT PITTORI
+    fastify.put('/pittori/:id', async function (request, reply) {
+        const id = request.params.id
+        const { nome, cognome } = request.body
+
+        const query = {
+            text: `UPDATE pittori SET nome = COALESCE($1, nome), cognome = COALESCE($2, cognome) WHERE cognome = $3`,
+            values: [nome, cognome, id]
+        }
+        try {
+            const { rows } = await client.query(query)
+            console.log(rows[0])
+            reply.code(204)
+            reply.send("PUT REQUEST DONE")
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+    //EDIT STANZE
+    fastify.put('/stanze/:id', async function (request, reply) {
+        const id = request.params.id
+        const { numero, descrizione } = request.body
+
+        const query = {
+            text: `UPDATE stanze SET numero = COALESCE($1, numero), descrizione = COALESCE($2, descrizione) WHERE numero = $3`,
+            values: [numero, descrizione, id]
+        }
+        try {
+            const { rows } = await client.query(query)
+            console.log(rows[0])
+            reply.code(204)
+            reply.send("PUT REQUEST DONE")
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
 }
 module.exports = routes
