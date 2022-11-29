98c006fb8efaa071f7f6929b371ca6be900610da
AlbertoVolpato <71636458+AlbertoVolpato@users.noreply.github.com>
2022-11-29 14:32:44 +0100

    .

---
+++ social_network/client/lib/screen/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  State<LoginPage> createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
