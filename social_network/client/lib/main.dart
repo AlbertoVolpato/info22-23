c5e5e391cb74140f960abe5aa7bdecc4f35f1cea
AlbertoVolpato <alberto.volpato@itiszuccante.edu.it>
2022-12-06 21:37:57 +0100

    .

--- social_network/client/lib/main.dart
+++ social_network/client/lib/main.dart
@@ -1,28 +1,15 @@
-import 'package:client/screen/home.dart';
 import 'package:flutter/material.dart';
-import 'package:client/screen/origin_screen.dart';
-import 'package:client/screen/homepage.dart';
-import 'package:client/screen/login.dart';
-import 'package:client/screen/register.dart';
+import 'package:client/screens/feed_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
-          //'/': (context) => OriginScreen(),
-          '/': (context) => HomePage(),
-          '/login': (context) => Login(),
-          '/register': (context) => SingUp(),
-          '/home': (context) => Home(),
-        },
-        theme: ThemeData(primarySwatch: Colors.grey));
+      title: 'Flutter Instagram Feed UI Redesign',
+      debugShowCheckedModeBanner: false,
+      home: FeedScreen(),
+    );
   }
 }
