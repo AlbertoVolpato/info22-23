ecb3c8633b74ef7b5c792c71f534f06247ac6520
AlbertoVolpato <alberto.volpato@itiszuccante.edu.it>
2022-12-10 15:01:50 +0100

    .

--- social_network/server/app.js
+++ social_network/server/app.js
@@ -1,42 +1,29 @@
-'use strict'
-
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
-//plugin personali
 const cors = require('@fastify/cors')
-//fine plugin personali
-
-
-// Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {}
 
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
 
-  // Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+     fastify = Fastify({
+        logger: true
+    })
+    // CommonJs
+     fastify = require('fastify')({
+        logger: true
+    })
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  });
+    // Declare a route
+    fastify.get('/', function (request, reply) {
+        reply.send({ hello: 'world' })
+    })
 
-  //aggiunta cors
-  fastify.register(cors, {
-    origin: '*'
-  })
-
-  //codice aggiunto da me
-  fastify.register(require('@fastify/postgres'), {
-    connectionString: 'postgres://postgres:Alberto@localhost/socialnetwork'
-  })
+    // Run the server!
+    fastify.listen({ port: 3000 }, function (err, address) {
+        if (err) {
+            fastify.log.error(err)
+            process.exit(1)
+        }
+        // Server is now listening on ${address}
+    })
 }
